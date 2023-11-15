b5fd2be078c0192380ce3906390d8582886a2845
vikasEvs <139117805+vikasEvs@users.noreply.github.com>
2023-11-15 12:11:44 +0530

    xml conversion code

---
+++ alloy.js
@@ -0,0 +1,102 @@
+//import Library to use in the code
+const fs = require("fs");
+const xlsx = require("xlsx");
+
+//read the file with name clientAlloy
+const workbook = xlsx.readFile("clientAlloy.xlsx");
+
+/** Select sheet where data is present, which we want to use */
+const sheet = workbook.Sheets["All Data"];
+// const sheet = workbook.Sheets[workbook.SheetNames[2]];
+
+/** Converting sheet data to json data */
+const data = xlsx.utils.sheet_to_json(sheet);
+
+/** Creating xmlString with initial text to come in xml file */
+let xmlString = `<exchange date="${new Date().toISOString()}" type="ALLOYS" schema-version="1.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="alloys-v1.2.xsd">`;
+
+//Some global variable
+let prevEdit = false;
+let prevDoc = false;
+
+/** Object of Symbols which need to be converted into there html entity */
+const symbolToEntity = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  "'": "&apos;",
+  '"': "&quot;"
+};
+
+/** This function will replace the symbols with there entities */
+function replaceSymbolsWithEntities(inputString) {
+  return inputString.replace(
+    /[&<>'"]/g,
+    match => symbolToEntity[match] || match
+  );
+}
+
+/** Here we will iterate to every row of excel to get data and insert into xml string */
+for (let i = 0; i < data.length; i++) {
+
+  /** First we are getting the data of Column "Fields" and "Components/data" */
+  let fieldName = data[i]["Fields"];
+  let fieldData = data[i]["Components/data"];
+
+  /** We will append data to xml string from different column only if data will present in this column "Components/data" of the row*/
+  if (fieldData) {
+    fieldData = replaceSymbolsWithEntities(fieldData); // checking all the text if there will be any symbol it will convert to the entity and return the string
+    if (i === 0 && fieldName === "PN") {
+      prevDoc = true;
+      xmlString += `<document pn="${fieldData}">`;
+    } else if (!prevDoc && fieldName === "PN") {
+      prevDoc = true;
+      xmlString += "</document>";
+      xmlString += `<document pn="${fieldData}">`;
+    }
+
+    if (fieldName === "EDIT" && fieldName !== "PRES" && fieldName !== "OPT") {
+      prevEdit = true;
+      xmlString += "<edit>";
+      const itemName = fieldData;
+      const startValue = data[i]["Min"];
+      const endValue = data[i]["Max"];
+      const isPresent = data[i]["Present"];
+      xmlString += `<item name="${itemName}" start="${startValue}" end="${endValue}" present="${isPresent}"/>`;
+    } else if (
+      !fieldName &&
+      prevEdit &&
+      fieldName !== "PRES" &&
+      fieldName !== "OPT"
+    ) {
+      prevEdit = true;
+      const itemName = fieldData;
+      const startValue = data[i]["Min"];
+      const endValue = data[i]["Max"];
+      const isPresent = data[i]["Present"];
+      xmlString += `<item name="${itemName}" start="${startValue}" end="${endValue}" present="${isPresent}"/>`;
+    } else if (
+      fieldName &&
+      fieldName !== "PN" &&
+      fieldName !== "PRES" &&
+      fieldName !== "OPT"
+    ) {
+      if (prevEdit) {
+        xmlString += "</edit>";
+        prevEdit = false;
+      }
+      xmlString += `<${fieldName.toLowerCase()}>${fieldData}</${fieldName.toLowerCase()}>`;
+      if (data.length !== i + 1 && data[i + 1]["Fields"] === "PN") {
+        prevDoc = false;
+      }
+    }
+  } else if (data.length !== i + 1 && data[i + 1]["Fields"] === "PN") {
+    prevDoc = false;
+  }
+}
+
+/** In last appending the closing tag in xml string */
+xmlString += "</document>";
+xmlString += "</exchange>";
+
+fs.writeFileSync("outputAlloy.xml", xmlString);
